686f3177c2f48b308c660fa8c4cc58fb08c47b96
Mahesh-Muke951 <mahesh.muke23@pccoepune.org>
2025-12-29 10:22:35 +0530

    Mark attendence page added and some ui changes

--- selection.js
+++ selection.js
@@ -1,50 +1,14 @@
+let fromDate=document.querySelector("#fromDate");
+let toDate=document.querySelector("#toDate");
 
-let selection_credentials = document.querySelector(".selection-credentials");
-let inputs = document.querySelectorAll("input");
+fromDate.addEventListener("click",()=>{
+    fromDate.showPicker();
+});
 
-selection_credentials.addEventListener("submit", function(eve){
-    eve.preventDefault();
+fromDate.addEventListener("change",()=>{
+    toDate.min=fromDate.value;
+})
 
-    let neww = document.createElement("div");
-    neww.classList.add("neww");
-
-
-    let form = document.createElement("form");
-
-    let inpsa = document.createElement("input");
-    inpsa.type = "text";
-    inpsa.placeholder = "enter period";
-    inpsa.value = inputs[0].value;
-
-    form.appendChild(inpsa);
-    
-
-    for(let i=0;i<4;i++)
-    {
-        let inpsb = document.createElement("input");
-        inpsb.type = "text";
-        inpsb.placeholder = `enter month ${i + 1}`;
-        inpsb.value = inputs[1 + i].value;
-        form.appendChild(inpsb);
-    }
-
-    for(let i=0;i<10;i++)
-    {
-        let inpsc = document.createElement("input");
-        inpsc.type = "text";
-        inpsc.placeholder = `enter subject name ${i + 1}`;
-        inpsc.value = inputs[5 + i].value;
-        form.appendChild(inpsc);
-    }
-
-    
-    
-
-    neww.appendChild(form);
-
-    selection_credentials.appendChild(neww);
-
-    
-
-
+toDate.addEventListener("click",()=>{
+    toDate.showPicker();
 });
